d97ebf7e71e0ee4c76535c086476af308b33ca2f
superlff888 <1310157572@qq.com>
2022-02-22 23:06:38 +0800

    删掉venv

--- Lesson_22/app自动化测试/录播/appium环境安装与架构介绍/appium_env_demo.py
+++ Lesson_22/app自动化测试/录播/appium环境安装与架构介绍/appium_env_demo.py
@@ -3,26 +3,29 @@
 # @Author  : ╰☆H.俠ゞ
 # =============================================================
 """
-遇到报错，还一个工程试试！！！可能文件夹中文太多了
+遇到报错，还一个工程试试！！！
+原因：可能是因为虚拟环境的原因
 """
+from time import sleep
 
 from appium import webdriver
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '6.0'
-desired_caps['deviceName'] = 'emulator-5554'  # 自定义，随便命名
-# com.android.settings/com.android.settings.Settings
+desired_caps['deviceName'] = '127.0.0.1：7555'  # 自定义，随便命名
 desired_caps['appPackage'] = 'com.xueqiu.android'
-desired_caps['appActivity'] = 'com.android.settings.Settings'
+desired_caps['appActivity'] = '.common.MainActivity'
+desired_caps['noReSet'] = 'true'  # 处理弹窗（点同意） ~ 记住之前的动作，住操作缓存信息
+desired_caps['dontStopAppOnReSet'] = 'true'  # 首次启动时，不停止app（相当于debugging）
+
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 el1 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
 el1.click()
-driver.implicitly_wait(3)
+driver.implicitly_wait(3)  # 存在于driver整个生命周期，全局（每个find军等待这个时间）
 el2 = driver.find_element_by_id("com.xueqiu.android:id/action_close")
 el2.click()
-driver.implicitly_wait(3)
 driver.back()
-driver.implicitly_wait(3)
+sleep(3)  # 强等待
