faf84394e3788f942ca5432b01076acf171d9dbd
superlff888 <1310157572@qq.com>
2022-02-23 13:04:47 +0800

    笔记

--- Lesson_22/app自动化测试/录播/appium环境安装与架构介绍/appium_env_demo.py
+++ Lesson_22/app自动化测试/录播/appium环境安装与架构介绍/appium_env_demo.py
@@ -9,23 +9,40 @@
 from time import sleep
 
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '6.0'
+# desired_caps['platformVersion'] = '6.0'
 desired_caps['deviceName'] = '127.0.0.1：7555'  # 自定义，随便命名
-desired_caps['appPackage'] = 'com.xueqiu.android'
-desired_caps['appActivity'] = '.common.MainActivity'
+# desired_caps['appPackage'] = 'com.xueqiu.android'
+# desired_caps['appActivity'] = '.common.MainActivity'
 desired_caps['noReSet'] = 'true'  # 处理弹窗（点同意） ~ 记住之前的动作，住操作缓存信息
 desired_caps['dontStopAppOnReSet'] = 'true'  # 首次启动时，不停止app（相当于debugging）
+desired_caps['skipDeviceInitialization'] = 'true'  # 首次启动时，不停止app（相当于web自动化的debugger；注意保证该页面元素能够定位到）
 
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-el1 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
-el1.click()
-driver.implicitly_wait(3)  # 存在于driver整个生命周期，全局（每个find军等待这个时间）
-el2 = driver.find_element_by_id("com.xueqiu.android:id/action_close")
+# 下面每一步都会等待
+driver.implicitly_wait(10)  # 存在于driver整个生命周期，全局（每个find军等待这个时间）
+# TouchAction(driver).tap(x=105, y=473).perform()
+el2 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
 el2.click()
+el3 = driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+el3.send_keys("alibaba")
+el4 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[7]/android.widget.LinearLayout/android.widget.TextView[1]")
+el4.click()
+
 driver.back()
-sleep(3)  # 强等待
+driver.back()
+driver.back()
+
+TouchAction(driver).tap(x=105, y=473).perform()
+el2 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+el2.click()
+el3 = driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+el3.send_keys("alibaba")
+el4 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[7]/android.widget.LinearLayout/android.widget.TextView[1]")
+el4.click()
+
+
