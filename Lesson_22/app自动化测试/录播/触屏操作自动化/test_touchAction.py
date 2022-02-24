40b45e0b542ae2b9bd65267309be8cd4dd61d479
superlff888 <1310157572@qq.com>
2022-02-24 16:28:28 +0800

    笔记

--- Lesson_22/app自动化测试/录播/触屏操作自动化/test_touchAction.py
+++ Lesson_22/app自动化测试/录播/触屏操作自动化/test_touchAction.py
@@ -2,6 +2,7 @@
 # @Time    : 2022/02/23 21:49
 # @Author  : ╰☆H.俠ゞ
 # =============================================================
+import os
 from time import sleep
 
 import pytest
@@ -13,18 +14,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 """
 场景一【 需要登录或者特别的状态，就不适合每个都重启，启动一次就好】
-    1、设置类setup_class；
+    1、设置类setup_class,登录放在setup方法中；
     2、在teardown()发那个发中，每个用例都返回首页，self.driver.back()
 
-    注意：和 desired_caps['noReset'] = 'true'，只是不清空app缓存情况下启动app；不能控制app启动次数
+    注意：和 desired_caps['noReset'] = 'true'，只是不清空app缓存情况下启动app；不能控制app启动次数，也不控制运行初始位置
 
 场景二【打开app时，出现弹窗，需要用desired_caps['noReset'] = 'true'】
     1、只需要添加参数 desired_caps['noReset'] = 'true'
 """
 
 
+def test_start():
+    os.system('adb shell am start -W -n com.xueqiu.android/.view.WelcomeActivityAlias -S')  # 打开app应用
+
+
 class TestDw:
-    def setup_class(self):
+    def setup(self):
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '6.0'
@@ -59,12 +64,13 @@ class TestDw:
 
     def test_search_2(self):
         self.driver.find_element_by_id("com.xueqiu.android:id/tv_search").click()
+        print(self.driver.find_element_by_id("com.xueqiu.android:id/tv_search"))
         self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴")
         locator = (By.XPATH, "//*[@resource-id='com.xueqiu.android:id/name' and @text='阿里巴巴']")
-        ES.element_to_be_clickable(locator)
-        # 方式1：WebDriverWait(self.driver, 10).until(ES.element_to_be_clickable(locator))
-        # 方式2：WebDriverWait类init方法中将self.driver转变为成员变量self._driver,until中的method调用成员变量self._driver
-        WebDriverWait(self.driver, 10).until(lambda x: x.find_element(*locator))  # 方法中一定要带一个参数，因为until中会将self._driver传给该方法
+        # 方式1：【利用expected_conditions】WebDriverWait(self.driver, 10).until(ES.element_to_be_clickable(locator))
+        # 方式2：【自定义函数】WebDriverWait类init方法中将self.driver转变为成员变量self._driver,until中的method调用成员变量self._driver
+        WebDriverWait(self.driver, 10).until(lambda x: x.find_element(*locator))  # 方法中一定要带一个参数x，因为until中会将self._driver传给该方法
+        self.driver.find_element(*locator).click()
 
     def test_touchAction(self):
         if self.driver.find_element_by_id("com.xueqiu.android:id/tv_search").is_enabled():
@@ -73,8 +79,7 @@ class TestDw:
 
     # 待优化
     def teardown(self):
-        if not self.driver.find_element_by_id("com.xueqiu.android:id/tv_search").is_displayed():
-            self.driver.back()
+        self.driver.quit()
 
 
 
--- Lesson_22/web_autoTest/隐式等待与显示等待/test_wait.py
+++ Lesson_22/web_autoTest/隐式等待与显示等待/test_wait.py
@@ -25,22 +25,26 @@ class TestWait:
         # self.driver.implicitly_wait(5)
 
     def test_wait(self):
-        # until()方法中要传一个方法method(即函数),
-        # 元素存在presence，但元素默认是隐藏
-        WebDriverWait(self.driver, 5).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#ember31")))
-        # 可见visibility，但可能还没加载完
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[id="ember31"]')))
+        """
+        # 方式1：【利用expected_conditions】WebDriverWait(self.driver, 10).until(ES.element_to_be_clickable(locator))
+        # 方式2：【自定义函数】WebDriverWait类init方法中将self.driver转变为成员变量self._driver,until中的method调用成员变量self._driver
+        # until()方法中要传一个方法method(即函数)，自定义的方法一定要带一个参数
+        """
 
-        # 自定义一个方法
-        def wait(x):  # 方法中一定要带一个参数，因为until中会将self._driver传给该方法
+        # 元素存在presence，但元素默认是隐藏
+        WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#ember31")))
+        # 可见visibility，但可能还没加载完
+        # WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[id="ember31"]')))
+
+        # 自定义一个方法；自定义的方法中一定要带一个参数，因为until中会将self._driver传给该方法(until方法中需要传入一个方法名)
+        def wait(x):
+            # 方法中一定要带一个参数，因为until中会将self._driver传给Method方法（查看源码），self._driver是在WebDriverWait类中转化的成员变量
             return len(x.find_elements(By.CSS_SELECTOR, "#ember31")) >= 1  # 判断元素的个数（长度）
         """
-        value = wait(self._driver)  # 
-                if value:  # 判断比较运算是否为真 ，概念模糊的话，print(2>1) ,比较运算的返回值为 True
-                    return value
+        【‘源码’ until()方法中】
+        value = wait(self._driver)
         """
-        # 方法中一定要带一个参数，因为until中会将self._driver传给该方法
+        # 自定义的方法中一定要带一个参数，因为until中会将self._driver传给该方法(until方法中需要传入一个方法名)
         WebDriverWait(self.driver, 5).until(wait)  # 传入方法名，不要带括号;而且此方法返回值为真，脚本才会结束等待，继续执行下面的代码
         self.driver.find_element(By.CSS_SELECTOR, "#ember31").click()
         print(self.driver.find_element(By.CSS_SELECTOR, "#ember31").location)  # {'x': 371, 'y': 75}
-
