0fa36b8585042d08566d262d25d29cbba897a87a
superlff888 <1310157572@qq.com>
2022-03-28 16:50:41 +0800

    笔记

--- Lesson_22/接口自动化测试/recorded_broadcast/request_and_assert/requests_mustache.py
+++ Lesson_22/接口自动化测试/recorded_broadcast/request_and_assert/requests_mustache.py
@@ -3,13 +3,26 @@
 # @Author  : ╰☆H.俠ゞ
 # =============================================================
 
-
-import requests
 import pystache  # 用两个花括号识别变量，可以对变量自动识别和替换
+
+env_temp = {
+    "default": "{{default}}",  # 通过env["default"]取值 【"dev": "127.0.0.1"】
+    "testing-studio":
+        {
+            "dev": "http",
+            "test": "{{test}}"
+        }
+}
+
+env = str(env_temp)
 
 
 class TestDemo:
     def test_demo(self):
-        j = pystache.render("Hi,{{person}}!", {"person": "lee"})  # 返回给定模板字符串，即template参数
+        j = pystache.render("{{say}},{{person}}!", **{"say": "Hi", "person": "lee"})  # 返回给定模板字符串，即template参数
         print(j)
 
+    def test_temp(self):
+        # template参数是string类型
+        j = pystache.render(env, **{"default": "test", "test": "127.0.0.1"})  # 返回给定模板字符串，即template参数
+        print(j)
