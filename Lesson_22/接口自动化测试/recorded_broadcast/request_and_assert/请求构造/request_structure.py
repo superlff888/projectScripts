4a900b10e2e26dbc7e97d3c06bd568d281165ee6
superlff888 <1310157572@qq.com>
2022-07-09 21:34:45 +0800

    up

--- Lesson_22/接口自动化测试/recorded_broadcast/request_and_assert/请求构造/request_structure.py
+++ Lesson_22/接口自动化测试/recorded_broadcast/request_and_assert/请求构造/request_structure.py
@@ -18,13 +18,13 @@ r.json() = r.encoding+r.content+content type json
 
 class Test_demo:
     # get请求 默认params入参 带参数
-    def test_demo(self):
+    def test_get_demo(self):
         payload = {"level": 1, "name": "seveniruby"}
-        r = requests.get("https://httpbin.testing-studio.com/get", params=payload)
+        r = requests.get("https://httpbin.testing-studio.com/get", payload)
         print(r.text)
 
     # get请求 params入参 带参数
-    def test_query(self):
+    def test_get_param(self):
         payload = {"level": 1, "name": "seveniruby"}
         r = requests.get("https://httpbin.testing-studio.com/get", params=payload)
         print(r.text)
@@ -33,6 +33,9 @@ class Test_demo:
 
     # post请求 data传参=form表单 dict词典格式
     def test_post_form(self):
+
+        """form表单形式传参  Content-Type:application/x-www-form-urlencoded"""
+
         data_payload = {"level": 1, "name": "seveniruby"}
         r = requests.post("https://httpbin.testing-studio.com/post", data=data_payload)
         print(r.text)
@@ -45,7 +48,7 @@ class Test_demo:
         print(r.cookies)
         assert r.json()['json']['level'] == 1
 
-    # 文件上传 仅模拟，无法执行
+    # 文件上传 （仅模拟，无法执行）
     def test_post_file(self):
         file = {"file": open("report.xls", 'rb')}
         r = requests.post("https://httpbin.testing-studio.com/post", files=file)
@@ -53,7 +56,7 @@ class Test_demo:
         print(r.cookies)
 
     # header 附加到请求headers中
-    def test_header(self):
+    def test_get_header(self):
         headers = {'H': 'HEADER_DEMO',
                    "User-Agent": "python-requests/2.25.8"}  # 覆盖或追加header信息
         r = requests.get("https://httpbin.testing-studio.com/get", headers=headers)  # 当人也可以post
@@ -61,14 +64,14 @@ class Test_demo:
         print(r.json()['headers']['H'])
 
     # cookies 通过cookies参数传递
-    def test_cookies(self):
+    def test_get_cookies(self):
         cookies = dict(cookies_are='working', teacher='AD')  # 需要传递字典参数 {'cookies_are': 'working'};可传递多个cookie
         print(cookies)
         r = requests.get("https://httpbin.testing-studio.com/get", cookies=cookies)  # 也可post
         print(r.text)
 
     # Cookie 通过headers进行传递
-    def test_Cookie(self):
+    def test_get_Cookie(self):
         headers = {'Cookie': 'Cookie_Demo',
                    "User-Agent": "python-requests/2.25.8"}  # 覆盖或追加header信息
         r = requests.get("https://httpbin.testing-studio.com/get", headers=headers)  # 也可post
