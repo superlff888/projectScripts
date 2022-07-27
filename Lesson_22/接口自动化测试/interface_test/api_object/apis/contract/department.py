03ee71cbe9c53c71be932912cde551d7ceccfd83
superlff888 <1310157572@qq.com>
2022-07-27 21:31:47 +0800

    up

--- Lesson_22/接口自动化测试/interface_test/api_object/apis/contract/department.py
+++ Lesson_22/接口自动化测试/interface_test/api_object/apis/contract/department.py
@@ -24,39 +24,26 @@ class Department(Wework):
         }
         # 发起请求
         res = requests.post(url=url, params=param, json=json_create_body)
-        print(res.json())
-        # 断言
-        # assert res.json()["errcode"] == 0
+        return res
 
-    def del_department(self, _id):
-        # 定义url
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/department/delete'
-        # 无论哪种请求方法，请求参数在url中，params传参
-        param = {
-            "access_token": self.token,
-            "id": _id
-        }
-        res = requests.get(url=url, params=param)
-        print(res.json())
-
-    def update_department(self):
+    def update_department(self, department_id):
         # 创建部门id
-        self.create_department(self.department_id)
+        self.create_department(department_id)
         # 定义url
         url = "https://qyapi.weixin.qq.com/cgi-bin/department/update"
         param = {
             "access_token": self.token
         }
         json_update_body = {
-            "name": f"广州研发中心_update_{self.department_id}",
-            "name_en": f"RDGZ_update_{self.department_id}",
+            "name": f"广州研发中心_update_{department_id}",
+            "name_en": f"RDGZ_update_{department_id}",
             "parentid": 1,
             "order": 1,
-            "id": self.department_id
+            "id": department_id
         }
         # 调用更新接口
         res = requests.post(url=url, params=param, json=json_update_body)
-        assert res.json()["errcode"] == 60020
+        return res.json()
 
     def query_department(self, _id):
         # 定义url
@@ -67,4 +54,15 @@ class Department(Wework):
             "id": _id
         }
         res = requests.get(url=url, params=param)
-        print(res.json())
+        return res.json()
+
+    def del_department(self, _id):
+        # 定义url
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/department/delete'
+        # 无论哪种请求方法，请求参数在url中，params传参
+        param = {
+            "access_token": self.token,
+            "id": _id
+        }
+        res = requests.get(url=url, params=param)
+        return res.json()
