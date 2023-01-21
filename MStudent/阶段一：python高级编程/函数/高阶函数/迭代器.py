c7cef883b9309cbe796fa1cab0a36c279507e7e5
superlff888 <1310157572@qq.com>
2023-01-21 13:12:52 +0800

    up

--- MStudent/阶段一：python高级编程/函数/高阶函数/迭代器.py
+++ MStudent/阶段一：python高级编程/函数/高阶函数/迭代器.py
@@ -12,9 +12,9 @@ list_ = [1, 2, 3, 4, 5, 6, 7, 8]
 # for i in list_:
 #     print(i)
 
-# next(list_)
-it = iter(list_)  # 返回迭代器的下一个项目;要和生成迭代器的 iter() 一起使用;迭代器与可迭代对象不是一个概念
-print(next(it))  # next()方法接收的是一个迭代器iterator，而不是可迭代对象iterable
+next(list_)  # next接收一个迭代器;返回迭代器的下一个项目
+it = iter(list_)  # iter() 函数用来生成迭代器; 一起使用;迭代器与可迭代对象不是一个概念
+print(next(it))  # next()方法接收的是一个迭代器iterator，而不是可迭代对象iterable;经常使用iter()先转化为迭代器
 print(next(it))
 
 print(isinstance('abc', (Iterator, )))  # 判断'abc'是不是迭代器iterator
