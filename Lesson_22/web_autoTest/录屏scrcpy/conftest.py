236cbbcf0efed2ee73190fe21b93d6f9c0c27d35
superlff888 <1310157572@qq.com>
2023-02-01 00:03:02 +0800

    up

--- Lesson_22/web_autoTest/录屏scrcpy/conftest.py
+++ Lesson_22/web_autoTest/录屏scrcpy/conftest.py
@@ -3,13 +3,33 @@
 # @Author  : ╰☆H.俠ゞ
 # =============================================================
 """
-适用于web和app
+1、安卓录屏工具 scrcpy
+   - 使用教程 https://blog.csdn.net/was172/article/details/99705855
+   - appium（adb）自带录屏功能，但是有可能对某些机型不支持，比如华为会有一些录屏限制
+2、适用于web和app
 """
-
+import os
+import signal
+import subprocess
+import sys
 
 import pytest
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module", autouse=True)  # 模块级别只调用一次
 def record_video():
-    command = "scrcpy --record tmp.mp4"
+    """
+    :shell : 为True时运行命令
+    :stdout : 标准信息输出
+    :stderr : 错误信息输出
+
+    sys.path.append(os.path.dirname(__file__))  # 动态添加环境变量
+    """
+    # 录屏命令
+    command = "scrcpy --record tmp.mp4"  # 需配置环境变量
+
+    # 执行命令"scrcpy --record tmp.mp4"，自动开启录屏；需将scrcpy.exe所在目录添加到环境变量
+    p = subprocess.Popen(args=command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(p)
+    yield '成功录屏'
+    os.kill(p.pid, signal.CTRL_C_EVENT)  # 命令行 Ctrl + C 结束录屏进程
